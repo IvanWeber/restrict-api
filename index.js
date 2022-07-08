ad743737c74dd6a8dbbbc18d6bb0894b961ab986
Ivan <ivanbdn@yandex.ru>
2022-07-08 10:57:11 +0300

    add full crud cycle

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
-
+const userRouter = require('./routes/user.routes')
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('HELLO POSTGRES + NODEJS!')
-})
+// app.get('/', (req, res) => {
+//     res.send('HELLO POSTGRES + NODEJS!')
+// })
+
+app.use(express.json())
+app.use('/api', userRouter)
 
 app.listen(PORT, () => console.log(`server has been started on port ${PORT} (http://localhost:${PORT})`))
